1631037fdb632efe95669305eeafc59d4e57d37e
lipsaandrei <lipsaandrei2003@gmail.com>
2026-03-31 20:25:02 +0300

    start

---
+++ app/src/main/java/com/example/smartbudget_asistent_financiar/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.smartbudget_asistent_financiar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.smartbudget_asistent_financiar.ui.theme.SmartBudget_Asistent_FinanciarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SmartBudget_Asistent_FinanciarTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SmartBudget_Asistent_FinanciarTheme {
+        Greeting("Android")
+    }
+}
